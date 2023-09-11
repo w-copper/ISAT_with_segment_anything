042d1345573b59c32310c42a357ba15c6de77fc2
copper <copper.wang@outlook.com>
2023-09-11 15:45:31 +0800

    更改为库方式导入

--- main.py
+++ main.py
@@ -3,7 +3,9 @@
 # import os
 
 from PyQt5 import QtWidgets
-from widgets.mainwindow import MainWindow
+import os
+os.environ['SAM_ANN_BASE_DIR'] = os.path.dirname(__file__)
+from sam_ann.widgets.mainwindow import MainWindow
 import sys
 
 
